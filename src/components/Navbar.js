3103e6add3e3a68aa17d2e95f38c1f69be7f12ae
rampratapkashyap <kashyaprampratap8@gmail.com>
2022-03-24 19:20:37 +0530

    new-Change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 export default function Navbar(props) {
+  console.log(props);
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
